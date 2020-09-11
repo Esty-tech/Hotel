9876d7dcb118db0423fc76476d9ef532a3468e59
Esty-tech <63051730+Esty-tech@users.noreply.github.com>
2020-09-11 15:02:41 -0300

    clima

---
+++ clima.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="https://widgets-viewer.climacell.co/v1/sdk.js"></script>
+<div class="climacell-widget" data-apikey="t9gK689aYinu5aAqQ4S9qm5Io3mnJBNF" data-type="hourly" 
+data-location-name="280 Summer Street, Boston, MA 02210, USA" data-location-lon="-71.04974909999999" 
+data-location-lat="42.3502782" data-size-mode="large" data-font-color="#ffa813" data-background-color="#bbe090" 
+data-font-family="georgia" data-weather-params="temp:C,precipitation:mm/hr,wind_speed:mph,humidity:%,cloud_cover:%" 
+data-precipitation-timeline="true" data-sunrise-sunset="true" data-allow-users-enter-address="true" ></div>
